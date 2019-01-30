4302a387adcfe6c8ba052e3510e22be2a7376474
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2019-01-30 20:00:11 +0900

    First tic-tac-toe app.

--- app.rb
+++ app.rb
@@ -2,23 +2,42 @@ require 'ovto'
 
 class MyApp < Ovto::App
   class State < Ovto::State
-    item :count, default: 0
+    item :board, default: [
+      [nil, nil, nil],
+      [nil, nil, nil],
+      [nil, nil, nil],
+    ]
+    item :player, default: 0
   end
 
   class Actions < Ovto::Actions
-    def increment_count(state:, num:)
-      return {count: state.count + num}
+    def update_board(state:, x:, y:)
+      new_board = state.board.map(&:dup)
+      new_board[y][x] = state.player
+      new_player = 1 - state.player
+      return {board: new_board, player: new_player}
     end
   end
 
   class MainComponent < Ovto::Component
+    PLAYER_MARK = { nil => "", 0 => '〇', 1 => '×' }
+
     def render(state:)
       o 'div' do
-        o 'h1', "Counter"
-        o 'div', state.count
-
-        o "input", type: "button", onclick:  ->(e){ actions.increment_count(num: 1) }, value: "+1"
-        o "input", type: "button", onclick:  ->(e){ actions.increment_count(num: 3) }, value: "+3"
+        o "div#player" do
+          "PLAYER: #{PLAYER_MARK[state.player]}"
+        end
+        o "table#board" do
+          state.board.each_with_index do |row, y|
+            o "tr" do
+              row.each_with_index do |cell, x|
+                o "td", onclick: ->(e){ actions.update_board(x: x, y: y) } do
+                  PLAYER_MARK[cell]
+                end
+              end
+            end
+          end
+        end
       end
     end
   end
