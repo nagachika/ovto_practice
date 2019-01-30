e54edf7a38229fc11d46d6cd495037efb6b625be
Chikanaga Tomoyuki <t-chikanaga@groovenauts.jp>
2019-01-30 19:47:32 +0900

    Implements counter sample.

---
+++ Rakefile
@@ -0,0 +1,4 @@
+
+task :build do
+  system("bundle exec opal -c -g ovto app.rb > app.js")
+end
--- app.rb
+++ app.rb
@@ -2,15 +2,23 @@ require 'ovto'
 
 class MyApp < Ovto::App
   class State < Ovto::State
+    item :count, default: 0
   end
 
   class Actions < Ovto::Actions
+    def increment_count(state:, num:)
+      return {count: state.count + num}
+    end
   end
 
   class MainComponent < Ovto::Component
     def render(state:)
       o 'div' do
-        o 'h1', "HELLO"
+        o 'h1', "Counter"
+        o 'div', state.count
+
+        o "input", type: "button", onclick:  ->(e){ actions.increment_count(num: 1) }, value: "+1"
+        o "input", type: "button", onclick:  ->(e){ actions.increment_count(num: 3) }, value: "+3"
       end
     end
   end
